88f7aff6be1d28711cc5f86442d6b5b236b09456
Samuel Quiroz <saqh5037@users.noreply.github.com>
2025-11-26 11:43:34 -0600

    fix(web): Change no-unused-vars to warning to pass CI

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -31,7 +31,7 @@ export default defineConfig([
       },
     },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'no-unused-vars': ['warn', { varsIgnorePattern: '^[A-Z_]', argsIgnorePattern: '^_' }],
     },
   },
 ])
